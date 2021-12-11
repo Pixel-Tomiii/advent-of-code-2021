f2cda5b0ac41307d0c8e5fa6f655df6f2b1c7390
Thomas <thomas27johnson39@gmail.com>
2021-12-11 05:42:09 +0000

    finish day 11

---
+++ Day 11/part 1.py
@@ -0,0 +1,70 @@
+import copy
+def get_data():
+    """Opens a text file called "input.txt" and reads all the data into a list.
+
+    Returns:
+    A list object containing each line in "input.txt"
+    """
+    with open("input.txt") as file:
+        lines = [list(map(int, list(line.rstrip()))) for line in file]
+    return lines
+
+data = get_data()
+
+# PART 1 START
+
+def output(grid):
+    print("\n".join([str(row) for row in grid]))
+    print()
+        
+def get_neighbours(grid, x, y):
+    neighbours = []
+
+    for offset_y in range(-1, 2):
+        for offset_x in range(-1, 2):
+            if offset_x == 0 and offset_y == 0:
+                continue
+            if (x + offset_x >= 0 and x + offset_x < len(grid[0]) and
+                    y + offset_y >= 0 and y + offset_y < len(grid)):
+                neighbours.append((x + offset_x, y + offset_y))
+    return neighbours
+
+def update(grid):
+    for y, row in enumerate(grid):
+        for x, octo in enumerate(row):
+            grid[y][x] += 1
+
+def flash_all(grid, flashed):
+    flash = False
+    total = 0
+    for y, row in enumerate(grid):
+        for x, octo in enumerate(row):
+            if grid[y][x] > 9:
+                flash = True
+                total += 1
+                flashed.add((x, y))
+                grid[y][x] = 0
+                neighbours = get_neighbours(grid, x, y)
+                for x, y in neighbours:
+                    if ((x, y)) not in flashed:
+                        grid[y][x] += 1
+
+                    
+    return flash, total
+
+total_flashes = 0
+output(data)
+for i in range(100):
+    flashed = set()
+    
+    update(data)
+    
+    while True:
+        flash, total = flash_all(data, flashed)
+        total_flashes += total
+        if not flash:
+            break
+        
+    total_flashes += total
+
+print("Total flashes:", total_flashes)
